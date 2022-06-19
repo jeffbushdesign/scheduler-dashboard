c5f28446e4e425f4b84bc92c81a2c5f26aec6f5b
Jeff Bush <jeffbushdesign@gmail.com>
2022-06-18 22:59:52 -0400

    Handle state

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 
 import classnames from "classnames";
+
 import Loading from "./Loading";
 import Panel from "./Panel";
 
@@ -30,11 +31,20 @@ const data = [
 
 class Dashboard extends Component {
   state = {
-    loading: false
+    loading: false,
+    focused: null
   };
 
+  selectPanel(id) {
+    this.setState({
+      focused: id
+    });
+  }
+
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
 
     if (this.state.loading) {
       return <Loading />;
@@ -48,16 +58,24 @@ class Dashboard extends Component {
     // The label can use the value from panel.label
     // The value can use the value from panel.value
 
-    const panels = data.map(panel => (
-      <Panel
-        key={panel.id}
-        id={panel.id}
-        label={panel.label}
-        value={panel.value}
-      />
-    ));
+    // State
+    // When the value is null it means we are in the unfocused four-panel view. The other options for the value of this.state.focused are the ids of the panels from 1 - 4. When we are in focused mode, we don't want to render four panels; we want to render one.
 
-    return <main className={dashboardClasses} />;
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+      .map(panel => (
+        <Panel
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.value}
+          onSelect={this.selectPanel}
+        />
+      ));
+
+    return <main className={dashboardClasses}>
+      {panels}
+
+    </main>;
   }
 }
 
