d7c26ea2477c3f40e7cbb7099b591a60cac2a05b
Jeff Bush <jeffbushdesign@gmail.com>
2022-06-18 23:15:55 -0400

    Bind .this

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -36,9 +36,9 @@ class Dashboard extends Component {
   };
 
   selectPanel(id) {
-    this.setState({
-      focused: id
-    });
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
   }
 
   render() {
@@ -68,7 +68,7 @@ class Dashboard extends Component {
           id={panel.id}
           label={panel.label}
           value={panel.value}
-          onSelect={this.selectPanel}
+          onSelect={event => this.selectPanel(panel.id)}
         />
       ));
 
